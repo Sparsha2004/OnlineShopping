b023ca6de70edc94116aea1063b677fecb237c33
Sparsha Poudel <1sparsha1poudel@gmail.com>
2025-07-31 10:35:56 +0545

    Add project files.

---
+++ OnlineShopping/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineShopping.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+        public string Description { get; set; } = string.Empty;
+        public int Price { get; set; }
+
+        // NOTE: Fix this to set the ProductPhoto as non-nullable
+        public string? ProductPhoto { get; set; }
+        public int CategoryId { get; set; }
+        
+        // Navigation property for related category
+        public virtual Category? Category { get; set; } = new Category();
+    }
+}
