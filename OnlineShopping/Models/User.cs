31ec1d1edc44bdc88724e144bc0ac77ef61e6de7
Sparsha Poudel <1sparsha1poudel@gmail.com>
2025-08-05 11:08:34 +0545

    Added requirements for Username and Password.

--- OnlineShopping/Models/User.cs
+++ OnlineShopping/Models/User.cs
@@ -1,9 +1,14 @@
-﻿namespace OnlineShopping.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [DataType(DataType.EmailAddress,ErrorMessage = "Invalid email address")]
         public string Username { get; set; }
+        [DataType(DataType.Password, ErrorMessage = "Invalid password")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password Length must be greater than 6 characters.")]
         public string Password { get; set; }
         public string UserType { get; set; } // Admin or Customer
 
