8239d8eb7e4821066def171d9eee2c55f579a4f8
Sparsha Poudel <1sparsha1poudel@gmail.com>
2025-08-05 10:46:39 +0545

    Created User.cs and added what was in the database

---
+++ OnlineShopping/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShopping.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; } // Admin or Customer
+
+    }
+}
