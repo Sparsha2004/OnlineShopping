d525c46811d868c43376a7e9bc018550a89690b4
Sparsha Poudel <1sparsha1poudel@gmail.com>
2025-08-06 10:45:40 +0545

    Added ConfirmPassword to register.cshtml

--- OnlineShopping/Models/RegisterViewModel.cs
+++ OnlineShopping/Models/RegisterViewModel.cs
@@ -9,5 +9,10 @@ namespace OnlineShopping.Models
         [DataType(DataType.Password, ErrorMessage = "Invalid password")]
         [StringLength(50, MinimumLength = 6, ErrorMessage = "Password Length must be greater than 6 characters.")]
         public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [DataType(DataType.Password, ErrorMessage = "Invalid password")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password Length must be greater than 6 characters.")]
+        public string ConfirmPassword { get; set; }
     }
 }
