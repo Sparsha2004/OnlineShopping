fbd051ae3428c7cdafa9624c882ea12d0fed37dc
Sparsha Poudel <1sparsha1poudel@gmail.com>
2025-08-06 10:22:13 +0545

    Created register view model and copied username, password to it.

---
+++ OnlineShopping/Models/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
+{
+    public class RegisterViewModel
+    {
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid email address")]
+        public string Username { get; set; }
+        [DataType(DataType.Password, ErrorMessage = "Invalid password")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password Length must be greater than 6 characters.")]
+        public string Password { get; set; }
+    }
+}
