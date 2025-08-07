88224f3c67c9f3e47e8693769da9beedbd7dd5b8
Sparsha Poudel <1sparsha1poudel@gmail.com>
2025-08-07 10:45:41 +0545

    Created LoginViewModel and kept same content as register except for confirm password

---
+++ OnlineShopping/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
+{
+    public class RegisterViewModel
+    {
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid email address")]
+        public string Username { get; set; }
+        [DataType(DataType.Password, ErrorMessage = "Invalid password")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password Length must be greater than 6 characters.")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [DataType(DataType.Password, ErrorMessage = "Invalid password")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password Length must be greater than 6 characters.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
